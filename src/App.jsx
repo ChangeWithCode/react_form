ff6208f310cfe885ef122d6091aa3af332c052f7
Qasim <qasimriaz814@gmail.com>
2023-06-18 03:06:01 +0500

    Cleaning of files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Buttons from "./Components/Buttons";
 import Fields from "./Components/Fields";
-import { data } from "autoprefixer";
 
 function App() {
   const [formData, setFormData] = useState({
